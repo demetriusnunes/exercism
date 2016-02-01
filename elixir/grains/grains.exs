6244a23a3a17733c29c0b3d90066791464f500b0
demetriusnunes <demetrius.nunes@izzui.com>
2016-02-01 15:37:22 -0200

    grains

---
+++ elixir/grains/grains.exs
@@ -0,0 +1,16 @@
+defmodule Grains do
+  @doc """
+  Calculate two to the power of the input minus one.
+  """
+  @spec square(pos_integer) :: pos_integer
+  def square(1), do: 1
+  def square(number), do: 2 * square(number - 1)
+
+  @doc """
+  Adds square of each number from 1 to 64.
+  """
+  @spec total :: pos_integer
+  def total do
+    Enum.reduce 1..64, 0, &(square(&1) + &2)
+  end
+end
